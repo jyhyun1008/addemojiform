be79d648d7b14340f17ba9cbe9bfbc49d0667853
jyhyun1008 <hyun1008@kasi.re.kr>
2023-12-19 16:28:53 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,118 @@
+var cssRoot = document.querySelector(':root');
+cssRoot.style.setProperty('--accent', themeColor);
+
+const head = document.head || document.getElementsByTagName('head')[0]
+var style = document.createElement('style');
+
+head.appendChild(style);
+
+style.type = 'text/css';
+if (style.styleSheet){
+  // This is required for IE8 and below.
+  style.styleSheet.cssText = customCss;
+} else {
+  style.appendChild(document.createTextNode(customCss));
+}
+
+const token = localStorage.getItem("token");
+const signedusername = localStorage.getItem("username");
+
+function getQueryStringObject() {
+    var a = window.location.search.substr(1).split('&');
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+        var p = a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+}
+
+var qs = getQueryStringObject();
+var page = qs.p;
+
+var isLogin = false;
+
+if (!page) {
+    if (token) {
+        document.querySelector('#page_title').innerText = instanceName+' 커스텀 이모지 등록 신청서'
+        document.querySelector('#page_content').innerHTML += '<div id="emojiform"><label for="emojiUrl">이미지 주소:</label><input id="emojiUrl" placeholder="https://~~~.png"></input><label for="emojiName">이름:</label><input id="emojiName" placeholder="영문 소문자와 숫자, 언더바(_)만 포함해주세요"></input><label for="emojiCategory">카테고리:</label><input id="emojiCategory" placeholder="장르 관련일 경우 제목 및 분류(애니, 게임 등)를 적어주세요."></input><label for="emojiTag">태그:</label><input id="emojiTag" placeholder="한글 위주로 작성해주세요. 공백으로 구분되며, 몇 가지 특수문자를 포함할 수 있습니다. 장르명, 캐릭터명도 함께 적어주세요."></input><input type="checkbox" name="checkbox" id="checkBox" /><label for="checkbox">피치타르트의 <a href="https://i.peacht.art/@admin/pages/content-rule">커스텀 이모지 추가 안내서</a>를 읽었으며, 동의하였습니다.</label><div class="button" id="submit">제출</div></div>'
+        
+        var emojiUrl = document.querySelector('#emojiUrl')
+        var emojiName = document.querySelector('#emojiName')
+        var emojiCategory = document.querySelector('#emojiCategory')
+        var emojiTag = document.querySelector('#emojiTag')
+        var checkBox = document.querySelector('#checkBox')
+
+        document.querySelector('#submit').addEventListener('click', function(e) {
+            if (emojiUrl.value == '') {
+                alert('이미지 주소를 입력하세요!')
+            } else if (emojiName.value == '') {
+                alert('이름을 입력하세요!')
+            } else if (emojiCategory.value == '') {
+                alert('카테고리를 입력하세요!')
+            } else if (emojiTag.value.split(' ').length == 0) {
+                alert('태그를 하나 이상 입력하세요!')
+            } else if (!checkBox.checked) {
+                alert('커스텀 이모지 추가 안내서에 동의해 주세요.')
+            } else {
+                const createNoteURL = 'https://' + instanceHost + '/api/notes/create'
+                const createNoteParam = {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        i: token,
+                        visibility: 'specified',
+                        cw: '커스텀 이모지 등록 신청',
+                        visibleUserIds: ['9gsr4vuc01'],
+                        text: '다음과 같이 커스텀 이모지 등록을 신청합니다.\n- emojiUrl: `'+emojiUrl.value+'`\n- emojiName: `'+emojiName.value+'`\n- emojiCategory: `'+emojiCategory.value+'`\n- emojiTag: `'+emojiTag.value+'`\n'
+                    })
+                }
+                fetch(createNoteURL, createNoteParam)
+                .then((noteData) => {return noteData.json()})
+                .then((noteRes) => {
+                    location.href = location.origin + '?p=done'
+                })
+            }
+        })
+    } else {
+        document.querySelector('#page_title').innerText = instanceName+' 커스텀 이모지 등록 신청서 - 로그인'
+        document.querySelector('#page_content').innerHTML += '<div id="signinform"><div>로그인이 필요합니다.</div><div>'+instanceName+' 아이디로 로그인할 수 있습니다.</div><div class="button" id="signin">로그인</div></div>'
+        document.querySelector('#signin').addEventListener('click', function(e) {
+            let uuid = self.crypto.randomUUID();
+            localStorage.setItem("sessionId", uuid);
+            var signinUrl = 'https://'+instanceHost+'/miauth/'+uuid+'?name=MiLog&callback=https%3A%2F%2F'+githubUserName+'.github.io%2F'+githubRepoName+'%3Fp%3Dcallback&permission=write:notes'
+            location.href = signinUrl;
+        })
+    }
+} else if (page == 'callback') {
+    const sessionId = localStorage.getItem("sessionId");
+    if (sessionId) {
+        var postUrl = 'https://'+instanceHost+'/api/miauth/'+sessionId+'/check'
+        var postParam = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({})
+        }
+        fetch(postUrl, postParam)
+        .then((tokenData) => {return tokenData.json()})
+        .then((tokenRes) => {
+            localStorage.setItem("token", tokenRes.token)
+            localStorage.setItem("username", tokenRes.user.username)
+            localStorage.setItem("userid", tokenRes.user.id)
+
+            location.href = location.origin
+        })
+        .catch(err => {throw err});
+    }
+} else if (page == 'done') {
+    document.querySelector('#page_title').innerText = instanceName+' 커스텀 이모지 등록 신청 완료'
+    document.querySelector('#page_content').innerHTML += '<div id="signinform">커스텀 이모지 등록 신청이 잘 완료되었습니다.</div>'
+}
---
+++ settings.js
@@ -0,0 +1,10 @@
+const instanceHost = 'i.peacht.art'
+const instanceName = '피치타르트'
+const themeColor = '#FF9899'
+const githubUserName = 'jyhyun1008'
+const githubRepoName = 'addemojiform'
+
+const customCss = `
+@import url('https://cdn.jsdelivr.net/npm/galmuri@latest/dist/galmuri.css');
+body, input::placeholder {font-family: Galmuri11;}
+`
