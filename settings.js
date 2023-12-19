1deb352f35b08ad7fc4590b282dff3d455cb302c
jyhyun1008 <hyun1008@kasi.re.kr>
2023-12-19 16:59:47 +0900

    Settings에서 새 창 열리도록 변경

--- settings.js
+++ settings.js
@@ -9,5 +9,5 @@ const customCss = `
 body, input::placeholder {font-family: Galmuri11;}
 `
 
-const ruleAgreement = '피치타르트의 <a href="https://i.peacht.art/@admin/pages/content-rule">커스텀 이모지 추가 안내서</a>를 읽었으며, 이에 동의합니다.'
-const DmAgreement = '본 양식을 제출하게 되면 지금 로그인된 계정으로 피치타르트 관리계정 <a href="https://i.peacht.art/@admin">@admin@i.peacht.art</a> 에게 다이렉트 메시지가 전송되는 것을 이해했으며, 이에 동의합니다.'
+const ruleAgreement = '피치타르트의 <a href="https://i.peacht.art/@admin/pages/content-rule" target="_blank">커스텀 이모지 추가 안내서</a>를 읽었으며, 이에 동의합니다.'
+const DmAgreement = '본 양식을 제출하게 되면 지금 로그인된 계정으로 피치타르트 관리계정 <a href="https://i.peacht.art/@admin" target="_blank">@admin@i.peacht.art</a> 에게 다이렉트 메시지가 전송되는 것을 이해했으며, 이에 동의합니다.'
